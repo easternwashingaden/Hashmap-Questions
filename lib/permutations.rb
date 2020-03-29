aadd1b83d7372cfbc22631720a429f71845da0ac
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-03-29 01:51:06 -0700

    completed permutations

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,24 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented"
+  return true if string1 == "" && string2 == ""
+  return false if (string1.length != string2.length) 
+
+
+  #convert both strings to arrays of letters
+  array1 = string1.chars
+  array2 = string2.chars
+
+  #create a hash by assigning each element of array1 for the keys of hash
+
+  hash = {}
+  array1.each do |letter|
+    hash[letter] = true
+  end
+
+  array2.each do |letter|
+    if hash[letter] != true
+      return false
+    end
+  end
+  return true
 end
