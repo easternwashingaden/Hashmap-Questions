ec658ddf6183a7b87cb6d58a87a215f0821a70f9
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-03-29 00:58:51 -0700

    completed palindrome

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,29 @@
 
 def palindrome_permutation?(string)
-  raise NotImplementedError, "palindrome_permutation? not implemented"
+  return true if string == ""
+
+  #Convert the string to an array of letters
+  string_array = string.chars
+
+  #reverse_string_array
+  reverse_string_array = []
+  string_array.each do |letter|
+    reverse_string_array.unshift(letter)
+  end
+
+  #create a string_hash by having each element string_array as keys
+  string_hash = {}
+ 
+  string_array.length.times do |i|
+    string_hash[string_array[i]] = reverse_string_array[i]
+  end
+  print string_hash
+
+  #compare each key and value of the hash
+  string_hash.each do |key, value|
+    if value == key
+      return true
+    end
+  end
+  return false
 end
