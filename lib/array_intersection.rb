e6b6e8aa18cce059b9c62328fcf82f7ce25a837b
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-03-28 21:03:10 -0700

    compeleted array-intersection

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,3 +1,36 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  
+  #create a hash with each element of the array as keys and value as true 
+  hash = {}
+  list1.each do |element|
+     hash[element] = true 
+  end
+  #find each element of list2 by the hash's keys
+  result = []
+  list2.each do |element|
+    if hash[element] == true
+      result << element
+    end
+  end
+  return result
 end
+
+# Without using the hash table 
+# def intersection(list1, list2) 
+#   result = []
+#   if list1.length >= list2.length
+#     list1.length.times do |i|
+#       if list1.include?(list2[i])
+#         result << list2[i]
+#       end
+#     end
+#     return result
+#   else
+#     list2.length.times do |i|
+#       if list2.include?(list1[i])
+#         result << list1[i]
+#       end
+#     end
+#     return result
+#   end
+# end
