90e54da7e68c97bda5a09a8cff06c8cf0b0dd795
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-03-29 16:51:20 -0700

    deleted an unneeded line of code!

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,9 +1,7 @@
 #Time complexity: O(n+m)
 #Space complexity: O(n)
 def permutations?(string1, string2)
-  return true if string1 == "" && string2 == ""
   return false if (string1.length != string2.length) 
-
 
   #convert both strings to arrays of letters
   array1 = string1.chars
